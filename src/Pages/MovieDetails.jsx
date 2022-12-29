6b8778342324037aa14ce1007915a1992d6f6cb3
adalid.bori <adalid.bori@TEPC-15>
2022-12-29 16:03:13 -0500

    terminado

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -1,7 +1,26 @@
-import movie from "../utils/movie_details.json";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import styles from "./MovieDetails.module.css";
-export function MovieDetails(){
+export function MovieDetails() {
+
+    const { movieId } = useParams();//useParams() -> Hook para obtener datos por parametro, el nombre del dato lo creamos en la clase App.jsx
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/movie/" + movieId, {
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Njc5ODExMzU3YjY1ZmIwNDQ1MTMwMDZjMDcyZDNhMCIsInN1YiI6IjYzYWRlYmEyYzU2ZDJkMDBkODQxZDQzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HCI1_xLI2h3cPK3P5qguVM32AetBPwAt-F6jf7AIbzQ',
+                "Content-Type": "application/json;charset=utf-8",
+            }
+        }).then(result => result.json()).then(data => {
+            setMovie(data)
+        })
+    }, [movieId]);
+    if (!movie) {
+        return null;
+    }
     const url = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+
     return (<div className={styles.detailsContainer}>
         <img className={`${styles.col} ${styles.movieImage}`} src={url} alt="" />
         <div className={`${styles.col} ${styles.detailsText}`}>
--- src/components/GeneralGrid.jsx
+++ src/components/GeneralGrid.jsx
@@ -1,12 +1,25 @@
 import { MovieGrid } from "./MovieGrid";
 import movies from "../utils/movies.json"
 import styles from "./GeneralGrid.module.css";
-export function GeneralGrid(){
+import { useEffect, useState } from "react";
+export function GeneralGrid() {
+    
+    const [movies, setMovies] = useState([]) // useState() hook para detectar cambios en el componente y volverlo a cargar
+    useEffect(() => {//efecto secundario para que se cargue despues del return
+        fetch("https://api.themoviedb.org/3/discover/movie", {
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5Njc5ODExMzU3YjY1ZmIwNDQ1MTMwMDZjMDcyZDNhMCIsInN1YiI6IjYzYWRlYmEyYzU2ZDJkMDBkODQxZDQzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HCI1_xLI2h3cPK3P5qguVM32AetBPwAt-F6jf7AIbzQ',
+                "Content-Type": "application/json;charset=utf-8",
+            }
+        }).then(result => result.json()).then(data => {
+            setMovies(data.results);
+        });
+    }, []);
     return (
         <div>
             <ul className={styles.moviesGrid}>
                 {movies.map(movie => (
-                   <MovieGrid key={movie.id} movie={movie} />
+                    <MovieGrid key={movie.id} movie={movie} />
                 ))}
             </ul>
         </div>
